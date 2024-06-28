bc12b55a67adaee76ff1ad36563c6e67d620a490
Alan Jafi <alan.jafi10@gmail.com>
2024-06-28 10:31:06 +0200

    comment

--- src/components/MyTask.vue
+++ src/components/MyTask.vue
@@ -3,6 +3,7 @@
     <v-card-title>{{ cards?.title }}</v-card-title>
     <v-card-text>
       <v-list>
+        <!-- Iterate through each card in the column -->
         <v-list-item
           v-for="(card, cardIndex) in cards?.cards"
           :key="card.id"
@@ -11,12 +12,14 @@
           @dragover.prevent
           @drop="onDrop"
         >
+          <!-- Render card details if not in edit mode -->
           <template v-if="!card.editing">
             <v-list-item-content @click="editCard(card, true)">
               <v-list-item-title>{{ card.title }}</v-list-item-title>
               <v-list-item-subtitle>{{ card.description }}</v-list-item-subtitle>
             </v-list-item-content>
           </template>
+          <!-- Render edit form if in edit mode -->
           <template v-else>
             <v-list-item-content>
               <v-text-field v-model="newTaskTitle" class="new-task-title-input" outlined dense></v-text-field>
@@ -25,12 +28,15 @@
               <v-btn class="custom-button" @click="editCard(card, false)">Cancel</v-btn>
             </v-list-item-content>
           </template>
+          <!-- Divider between cards if not the last one -->
           <v-divider v-if="cardIndex < cards?.cards.length - 1"></v-divider>
         </v-list-item>
       </v-list>
+      <!-- Input field for adding a new task title and button to add task -->
       <v-text-field v-model="newTaskTitle" label="New Task Title" outlined dense class="custom-text-field"></v-text-field>
       <v-btn class="custom-button" @click="addTask">Add Task</v-btn>
     </v-card-text>
+    <!-- Button to remove cards  -->
     <v-card-actions>
       <v-btn class="custom-button" @click="removeCards">Remove Card</v-btn>
     </v-card-actions>
@@ -40,19 +46,22 @@
 <script setup lang="ts">
 import { ref, defineProps, defineEmits } from 'vue';
 
+// Define props passed to the component
 const props = defineProps<{
-  cards: Record<string, any>;
-  columnIndex: number;
+  cards: Record<string, any>; 
+  columnIndex: number; 
 }>();
 
 const emit = defineEmits(['remove-cards', 'add-task', 'drag-start', 'drop', 'edit-card']);
 
 const newTaskTitle = ref('');
 
+//handle drag start event
 const onDragStart = (card: any) => {
   emit('drag-start', card, props.cards);
 };
 
+//andle drop event
 const onDrop = () => {
   if (props.cards) {
     emit('drop', props.cards);
@@ -63,10 +72,12 @@ const editCard = (card: any, isEditing: boolean) => {
   emit('edit-card', card, isEditing);
 };
 
+//Save edits made to a card
 const saveEdit = (card: any) => {
   editCard(card, false);
 };
 
+//Add new task to the card
 const addTask = () => {
   if (props.cards) {
     emit('add-task', props.columnIndex, newTaskTitle.value.trim());
